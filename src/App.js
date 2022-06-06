2ec0b4c44b07e9cb9a681ddb732916a48847f06a
burhan-aj <burhanuddin-a@thecareerlabs.com>
2022-06-06 12:39:19 +0530

    as

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 
 function App() {
@@ -12,7 +13,6 @@ function App() {
       <About />
       <Skills />
       <Work />
-      <Contact />
     </div>
   );
 }
