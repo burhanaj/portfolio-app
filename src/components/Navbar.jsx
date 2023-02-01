c5de8400de4a0a8ea6a28f53ffbd4eb14d80bae1
Burhanuddin A <burhanajmer95@gmail.com>
2023-02-01 14:39:52 +0530

    🦾

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <ul className='hidden md:flex'>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
+                <li><a href="#skills">Skill</a></li>
                 <li><a href="#work">Work</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
