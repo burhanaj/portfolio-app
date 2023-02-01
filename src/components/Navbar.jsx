e1237dcbba8c5a533e267203e9bfb5cbfb782045
Burhanuddin A <burhanajmer95@gmail.com>
2023-02-01 14:43:55 +0530

    🦿

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <ul className='hidden md:flex'>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skill</a></li>
+                <li><a href="#skills">Skills</a></li>
                 <li><a href="#work">Work</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
@@ -27,7 +27,7 @@ const Navbar = () => {
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0c1c35] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'><a href="#home">Home</a></li>
                 <li className='py-6 text-4xl'><a href="#about">About</a></li>
-                <li className='py-6 text-4xl'><a href="#skills">Skills </a></li>
+                <li className='py-6 text-4xl'><a href="#skills">Skills</a></li>
                 <li className='py-6 text-4xl'><a href="#work">Work</a></li>
                 <li className='py-6 text-4xl'><a href="#contact">Contact</a></li>
         </ul>
